73311d17f653f904945e4b8769634f12ce3d2fc4
manuqiao <manuqiao@me.com>
2013-06-14 21:05:13 +0800

    Initial Commit

---
+++ windmill/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  windmill
+//
+//  Created by ManuQiao on 13-6-14.
+//  Copyright manu 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ windmill/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  windmill
+//
+//  Created by ManuQiao on 13-6-14.
+//  Copyright manu 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
